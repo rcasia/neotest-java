2b9fecbea7aaeff0e2a0ba1919a9d4cf2ecb85ad
Ricardo Casía <ricardocasia.dev@gmail.com>
2023-05-27 22:46:50 +0200

    formatted code running stylua

--- lua/neotest-java/core/result_builder.lua
+++ lua/neotest-java/core/result_builder.lua
@@ -5,29 +5,27 @@
 ---@field errors? neotest.Error[]
 
 ResultBuilder = {}
-  ---@async
-  ---@param spec neotest.RunSpec
-  ---@param result neotest.StrategyResult
-  ---@param tree neotest.Tree
-  ---@return table<string, neotest.Result>
-  function ResultBuilder.build_results(spec, result, tree)
-    local results = {}
+---@async
+---@param spec neotest.RunSpec
+---@param result neotest.StrategyResult
+---@param tree neotest.Tree
+---@return table<string, neotest.Result>
+function ResultBuilder.build_results(spec, result, tree)
+	local results = {}
 
-    results["prueba"] = {
-      status = "skipped",
-      output = "output",
-      short = "short",
-      errors = {
-        {
-          message = "message",
-          trace = "trace"
-        }
-      }
-    }
+	results["prueba"] = {
+		status = "skipped",
+		output = "output",
+		short = "short",
+		errors = {
+			{
+				message = "message",
+				trace = "trace",
+			},
+		},
+	}
 
-    return results
-  end
-
+	return results
+end
 
 return ResultBuilder
-
--- tests/core/spec_builder_spec.lua
+++ tests/core/spec_builder_spec.lua
@@ -3,33 +3,29 @@ local plugin = require("neotest-java")
 local Tree = require("neotest.types.tree")
 
 describe("SpecBuilder", function()
+	it("builds the spec", function()
+		local args = {
+			tree = {
+				data = function()
+					return {
+						path = "/home/user/project/src/test/java/com/example/ExampleTest.java",
+						name = "test1",
+					}
+				end,
+			},
+			extra_args = {},
+		}
 
-  it("builds the spec", function()
-    local args = {
-      tree = {
-        data = function()
-          return {
-            path = "/home/user/project/src/test/java/com/example/ExampleTest.java",
-            name = "test1"
-          }
-        end
-      },
-      extra_args = {},
-    }
-   
-    -- when
-    local actual = plugin.build_spec(args)
+		-- when
+		local actual = plugin.build_spec(args)
 
-  
-    -- then
-    local expected_position = "com.example.ExampleTest#test1"
+		-- then
+		local expected_position = "com.example.ExampleTest#test1"
 
-    local expected_command = "mvn test -Dtest=" .. expected_position
-    local expected_cwd = "/home/user/project"
+		local expected_command = "mvn test -Dtest=" .. expected_position
+		local expected_cwd = "/home/user/project"
 
-    assert.are.equal(expected_command, actual.command)
-    assert.are.equal(expected_cwd, actual.cwd)
-
-  end)
+		assert.are.equal(expected_command, actual.command)
+		assert.are.equal(expected_cwd, actual.cwd)
+	end)
 end)
-
