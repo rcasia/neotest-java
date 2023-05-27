436942f8fb6a2e2b075d7c3040f259225241dba8
Ricardo Casía <ricardocasia.dev@gmail.com>
2023-05-27 22:46:50 +0200

    formatted code running stylua

--- tests/core/result_builder_spec.lua
+++ tests/core/result_builder_spec.lua
@@ -3,32 +3,28 @@ local plugin = require("neotest-java")
 local Tree = require("neotest.types.tree")
 
 describe("ResultBuilder", function()
+	it("builds the results", function()
+		--given
+		local runSpec = {
+			command = [[/usr/bin/java]],
+			env = {},
+			cwd = [[/home/runner/work/neotest-java/neotest-java]],
+			context = {},
+			strategy = "java",
+			stream = function() end,
+		}
 
-  it("builds the results", function()
-    --given
-    local runSpec = {
-      command = [[/usr/bin/java]],
-      env = {},
-      cwd = [[/home/runner/work/neotest-java/neotest-java]],
-      context = {},
-      strategy = "java",
-      stream = function() end
-    }
+		local strategyResult = {
+			code = 0,
+			output = "output",
+		}
 
-    local strategyResult = {
-      code = 0,
-      output = "output"
-    }
+		local tree = {}
 
-    local tree = {}
+		--when
+		local actual = plugin.results(runSpec, strategyResult, tree)
 
-    --when
-    local actual = plugin.results(runSpec, strategyResult, tree)
-
-    --then
-    print(vim.inspect(actual))
-
-
-  end)
+		--then
+		print(vim.inspect(actual))
+	end)
 end)
-
--- tests/core/root_finder_spec.lua
+++ tests/core/root_finder_spec.lua
@@ -3,27 +3,26 @@ local plugin = require("neotest-java")
 local Tree = require("neotest.types").Tree
 
 describe("RootFinder", function()
-  
-  it("should find the root of a project", ( function()
-    -- given 
-    local absoluteDirs = {
-      "/home/user/project/src/main/java",
-      "/home/user/project/src/main/resources",
-      "/home/user/project/src/test/java",
-      "/home/user/project/src/test/resources",
-    }
+	it(
+		"should find the root of a project",
+		function()
+			-- given
+			local absoluteDirs = {
+				"/home/user/project/src/main/java",
+				"/home/user/project/src/main/resources",
+				"/home/user/project/src/test/java",
+				"/home/user/project/src/test/resources",
+			}
 
-    local expectedRoot = "/home/user/project"
-    
-    -- when
-    for _, dir in ipairs(absoluteDirs) do
-      local actualRoot = plugin.root(dir)
+			local expectedRoot = "/home/user/project"
 
-      -- then
-      assert.are.same(actualRoot, expectedRoot)
-    end
+			-- when
+			for _, dir in ipairs(absoluteDirs) do
+				local actualRoot = plugin.root(dir)
 
-  end))
-
+				-- then
+				assert.are.same(actualRoot, expectedRoot)
+			end
+		end
+	)
 end)
-
