0c07ff2000e8e02cc65418dd835a6be5be2df9bf
Ricardo Casía <ricardocasia.dev@gmail.com>
2023-05-28 22:30:35 +0200

    improved query for positions discoverer

--- tests/core/positions_discoverer_spec.lua
+++ tests/core/positions_discoverer_spec.lua
@@ -14,11 +14,13 @@ describe("PositionsDiscoverer", function()
 		local actual_list = actual:to_list()
 
 		-- for debugging
-		-- print(actual)
-		-- print("value: " .. actual_list[3][1].name)
+		print(actual)
+		print("value: " .. actual_list[3][1].name)
 
 		assert.equals(actual_list[2][1].name, "shouldFindThis1")
 		assert.equals(actual_list[3][1].name, "shouldFindThis2")
 		assert.equals(actual_list[4][1].name, "shouldFindThis3")
+		assert.equals(actual_list[5][1].name, "shouldFindThis4")
+
 	end)
 end)
