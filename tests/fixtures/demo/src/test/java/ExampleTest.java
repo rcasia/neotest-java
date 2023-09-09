ae17db308ff1e826e298eedefa1393d1e187d66b
Ricardo Casía <ricardocasia.dev@gmail.com>
2023-09-09 20:34:31 +0200

    added demo java project

---
+++ tests/fixtures/demo/src/test/java/ExampleTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExampleTest {
+    @Test
+    void shouldNotFail() {
+        assertTrue(true);
+    }
+    
+    @Test
+    void shouldFail() {
+        assertTrue(false);
+    }
+}
