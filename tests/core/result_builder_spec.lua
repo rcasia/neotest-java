d3436fa3dcd5df1eda74a06fe02dac274366d2a7
Ricardo Casía <ricardocasia.dev@gmail.com>
2023-09-11 01:18:03 +0200

    fix: test single methods

--- tests/core/result_builder_spec.lua
+++ tests/core/result_builder_spec.lua
@@ -5,17 +5,20 @@ local function getCurrentDir()
 	return vim.fn.fnamemodify(vim.fn.expand("%:p:h"), ":p")
 end
 
+-- Function to convert a Lua table to a string
+function tableToString(tbl)
+	return vim.inspect(tbl)
+end
+
+function assert_equal_ignoring_whitespaces(expected, actual)
+	assert.are.equal(expected:gsub("%s+", ""), actual:gsub("%s+", ""))
+end
+
 describe("ResultBuilder", function()
 	async.it("builds the results", function()
 		--given
 		local runSpec = {
-			-- TODO: use a real test runner
-			command = [[/usr/bin/java]],
-			env = {},
 			cwd = getCurrentDir() .. "tests/fixtures/demo",
-			context = {},
-			strategy = "java",
-			stream = function() end,
 		}
 
 		local strategyResult = {
@@ -27,9 +30,55 @@ describe("ResultBuilder", function()
 		local tree = plugin.discover_positions(file_path)
 
 		--when
-		local actual = plugin.results(runSpec, strategyResult, tree)
+		local results = plugin.results(runSpec, strategyResult, tree)
 
 		--then
-		print(vim.inspect(actual))
+		local actual = tableToString(results)
+		local expected = [[
+      {
+        ["{{currentDir}}tests/fixtures/demo/src/test/java/com/example/ExampleTest.java::shouldFail"] = {
+          status = "failed"
+        },
+        ["{{currentDir}}tests/fixtures/demo/src/test/java/com/example/ExampleTest.java::shouldNotFail"] = {
+          status = "passed"
+        }
+      }
+    ]]
+
+		expected = expected:gsub("{{currentDir}}", getCurrentDir())
+
+		assert_equal_ignoring_whitespaces(expected, actual)
+	end)
+
+	async.it("builds the results when the is a single test method and it fails", function()
+		--given
+		local runSpec = {
+			cwd = getCurrentDir() .. "tests/fixtures/demo",
+		}
+
+		local strategyResult = {
+			code = 0,
+			output = "output",
+		}
+
+		local file_path = getCurrentDir()
+			.. "tests/fixtures/demo/src/test/java/com/example/SingleMethodFailingTest.java"
+		local tree = plugin.discover_positions(file_path)
+
+		--when
+		local results = plugin.results(runSpec, strategyResult, tree)
+
+		--then
+		local actual = tableToString(results)
+		local expected = [[
+    {
+      ["{{currentDir}}tests/fixtures/demo/src/test/java/com/example/SingleMethodFailingTest.java::shouldFail"] 
+
+      = { status = "failed" }
+    }
+    ]]
+		expected = expected:gsub("{{currentDir}}", getCurrentDir())
+
+		assert_equal_ignoring_whitespaces(expected, actual)
 	end)
 end)
