e1b8070bc0a2ec2f286364c8de1bc647315e6fee
reevo <reevor@gmail.com>
2024-03-24 18:02:30 +0530

    Tests can now be found via annotations if the name matching fails.

--- lua/neotest-java/core/file_checker.lua
+++ lua/neotest-java/core/file_checker.lua
@@ -1,24 +1,40 @@
+local File = require("neotest.lib.file")
 FileChecker = {
 	test_file_patterns = {
 		"IT%.java$",
 		"Test%.java$",
 		"Tests%.java$",
+		"Spec%.java$",
 		"IntegrationTest%.java$",
 		"IntegrationTests%.java$",
 	},
+	test_file_body = {
+		"@Test",
+	},
 }
+local function fileBodyContainsPattern(file_path)
+	for _, annotation in ipairs(FileChecker.test_file_body) do
+		if string.find(File.read(file_path), annotation) then
+			return true
+		end
+	end
+	return false
+end
 
----@async
----@param file_path string
----@return boolean
-function FileChecker.isTestFile(file_path)
+local function fileNameMatchesPattern(file_path)
 	for _, pattern in ipairs(FileChecker.test_file_patterns) do
 		if string.find(file_path, pattern) then
 			return true
 		end
 	end
-
 	return false
+end
+
+---@async
+---@param file_path string
+---@return boolean
+function FileChecker.isTestFile(file_path)
+	return fileNameMatchesPattern(file_path) or fileBodyContainsPattern(file_path)
 end
 
 return FileChecker
