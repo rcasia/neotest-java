21a2c37d2409fd64230ca6948c1145d1b8300e64
reevo <reevor@gmail.com>
2024-03-26 11:25:05 +0530

    added tests

--- lua/neotest-java/core/file_checker.lua
+++ lua/neotest-java/core/file_checker.lua
@@ -11,19 +11,31 @@ FileChecker = {
 	test_file_body = {
 		"@Test",
 	},
+	test_base_file_path = {
+		"src/test",
+	},
 }
-local function fileBodyContainsPattern(file_path)
-	for _, annotation in ipairs(FileChecker.test_file_body) do
-		if string.find(File.read(file_path), annotation) then
+local function fileNameMatchesPattern(file_path)
+	for _, pattern in ipairs(FileChecker.test_file_patterns) do
+		if string.find(file_path, pattern) then
 			return true
 		end
 	end
 	return false
 end
 
-local function fileNameMatchesPattern(file_path)
-	for _, pattern in ipairs(FileChecker.test_file_patterns) do
-		if string.find(file_path, pattern) then
+local function packagePathMatchesPattern(file_path)
+	for _, path in ipairs(FileChecker.test_base_file_path) do
+		if string.find(file_path, path) then
+			return true
+		end
+	end
+	return false
+end
+
+local function fileBodyContainsPattern(file_path)
+	for _, annotation in ipairs(FileChecker.test_file_body) do
+		if string.find(File.read(file_path), annotation) then
 			return true
 		end
 	end
@@ -34,7 +46,8 @@ end
 ---@param file_path string
 ---@return boolean
 function FileChecker.isTestFile(file_path)
-	return fileNameMatchesPattern(file_path) or fileBodyContainsPattern(file_path)
+	return fileNameMatchesPattern(file_path)
+		or (packagePathMatchesPattern(file_path) and fileBodyContainsPattern(file_path))
 end
 
 return FileChecker
--- tests/minimal_init.vim
+++ tests/minimal_init.vim
@@ -2,5 +2,6 @@ set runtimepath+=.
 set runtimepath+=./deps/plenary.nvim
 set runtimepath+=./deps/neotest
 set runtimepath+=./deps/nvim-treesitter
+set runtimepath+=~/.local/share/nvim/lazy/nvim-nio
 runtime! plugin/plenary.vim
 
