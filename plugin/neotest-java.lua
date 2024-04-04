e3aab79d071643ef0514687d82f82d3b2f8744c7
Ricardo Casía <ricardocasia.dev@gmail.com>
2024-04-04 21:29:56 +0200

    Fix setup function (#91)
    Related to issue #89 
    
    * fix: create data directory on init
    
    * fix: false error on setup
    
    * refator: introduce variable for junit jar

--- plugin/neotest-java.lua
+++ plugin/neotest-java.lua
@@ -1,10 +1,13 @@
 local exists = require("neotest.lib.file").exists
 local job = require("plenary.job")
+local logger = require("neotest.logging")
+local lib = require("neotest.lib")
 
 local options = {
 	setup = function()
-		if exists(vim.fn.stdpath("data") .. "/neotest-java/junit-platform-console-standalone-1.10.1.jar") then
-			print("Already setup!")
+		local filepath = vim.fn.stdpath("data") .. "/neotest-java/junit-platform-console-standalone-1.10.1.jar"
+		if exists(filepath) then
+			lib.notify("Already setup!")
 			return
 		end
 
@@ -15,19 +18,23 @@ local options = {
 				command = "curl",
 				args = {
 					"--output",
-					vim.fn.stdpath("data") .. "/neotest-java/junit-platform-console-standalone-1.10.1.jar",
+					filepath,
 					"https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/1.10.1/junit-platform-console-standalone-1.10.1.jar",
 				},
 				on_stderr = function(_, data)
 					table.insert(stderr, data)
 				end,
+				on_exit = function(_, code)
+					if code == 0 then
+						lib.notify("Downloaded Junit Standalone successfully!")
+					else
+						local output = table.concat(stderr, "\n")
+						lib.notify(string.format("Error while downloading: \n %s", output), "error")
+						logger.error(output)
+					end
+				end,
 			})
-			:sync(10000)
-
-		-- if any error
-		if #stderr ~= 0 then
-			error(table.concat(stderr, "\n"))
-		end
+			:start()
 	end,
 }
 
