2b16cf2e052ad6decfc01f9fd55ca5f02c4387a6
Ricardo Casía <ricardocasia.dev@gmail.com>
2024-06-22 18:08:37 +0200

    fix: problems when cwd changes (#110)
    * feat: created context_holder
    
    * fix: problems running tests when cwd accidentally changes

--- lua/neotest-java/init.lua
+++ lua/neotest-java/init.lua
@@ -9,9 +9,9 @@ local position_discoverer = require("neotest-java.core.positions_discoverer")
 local spec_builder = require("neotest-java.core.spec_builder")
 local result_builder = require("neotest-java.core.result_builder")
 local log = require("neotest-java.logger")
+local ch = require("neotest-java.context_holder")
 
 local detect_project_type = require("neotest-java.util.detect_project_type")
-local there_is_wrapper_in = require("neotest-java.util.there_is_wrapper_in")
 
 local check_junit_jar = function(filepath)
 	local exists, err = File.exists(filepath)
@@ -28,11 +28,6 @@ end
 ---@class neotest.Adapter
 NeotestJavaAdapter = {
 	name = "neotest-java",
-	project_type = "maven", -- default to maven
-	config = {
-		ignore_wrapper = false,
-		junit_jar = vim.fn.stdpath("data") .. "/neotest-java/junit-platform-console-standalone-1.10.1.jar",
-	},
 }
 
 ---Find the project root directory given a current directory to work from.
@@ -41,7 +36,11 @@ NeotestJavaAdapter = {
 ---@param dir string @Directory to treat as cwd
 ---@return string | nil @Absolute root dir of test suite
 function NeotestJavaAdapter.root(dir)
-	return root_finder.find_root(dir)
+	local root = root_finder.find_root(dir)
+	if root then
+		ch.set_root(root)
+	end
+	return root
 end
 
 ---Filter directories when searching for test files
@@ -73,24 +72,19 @@ end
 ---@return nil | neotest.RunSpec | neotest.RunSpec[]
 function NeotestJavaAdapter.build_spec(args)
 	local self = NeotestJavaAdapter
-	check_junit_jar(self.config.junit_jar)
+	check_junit_jar(ch.get_context().config.junit_jar)
 
 	-- TODO: find a way to avoid to make this steps every time
 
 	-- find root
-	local root = self.root(args.tree:data().path)
+	local root = ch.get_context().root or self.root(vim.fn.getcwd())
+	assert(root, "root directory not found")
 
 	-- detect project type
-	self.project_type = detect_project_type(root)
-
-	-- decide to ignore wrapper or not
-	local ignore_wrapper = self.config.ignore_wrapper
-	if not ignore_wrapper then
-		ignore_wrapper = not there_is_wrapper_in(root)
-	end
+	local project_type = detect_project_type(root)
 
 	-- build spec
-	return spec_builder.build_spec(args, self.project_type, ignore_wrapper, self.config)
+	return spec_builder.build_spec(args, project_type, ch.get_context().config)
 end
 
 ---@async
@@ -114,8 +108,9 @@ end)()
 setmetatable(NeotestJavaAdapter, {
 	__call = function(_, opts)
 		opts = opts or {}
-		local config = NeotestJavaAdapter.config or {}
-		NeotestJavaAdapter.config = vim.tbl_extend("force", config, opts)
+
+		ch.set_opts(opts)
+
 		return NeotestJavaAdapter
 	end,
 })
