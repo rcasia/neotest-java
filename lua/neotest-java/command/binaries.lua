a9cbc94fe03fb4cad4741143ea9b42c805d51601
Massimo Gengarelli <massimo.gengarelli@proton.me>
2024-06-27 17:49:15 +0200

    fix: ignore wrappers (#119)
    Hello,
    In NixOS it is impossible to use the wrappers from mvn and gradle and we
    have to always use the binaries which are installed via the derivations.
    
    It looks like the `ignore_wrappers` option was ignored completely, and
    there was also a small bug when setting the options (it seems to me the
    API for `vim.tbl_extend` has changed, and it now returns a table instead
    of modifying the table in-place). I fixed both issues in this MR.
    Please tell me if there's something more I can do :)

--- lua/neotest-java/command/binaries.lua
+++ lua/neotest-java/command/binaries.lua
@@ -1,4 +1,5 @@
 local File = require("neotest.lib.file")
+local ch = require("neotest-java.context_holder")
 local runtime = require("neotest-java.command.runtime")
 
 local binaries = {
@@ -13,14 +14,14 @@ local binaries = {
 	end,
 
 	mvn = function()
-		if File.exists("mvnw") then
+		if File.exists("mvnw") and not ch.get_context().config.ignore_wrapper then
 			return "./mvnw"
 		end
 		return "mvn"
 	end,
 
 	gradle = function()
-		if File.exists("gradlew") then
+		if File.exists("gradlew") and not ch.get_context().config.ignore_wrapper then
 			return "./gradlew"
 		end
 		return "gradle"
