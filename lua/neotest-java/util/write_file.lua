16cbd06f05a2c1d17cfc65e80e7b81f23594be1b
Ricardo Casía Moka <ricardocasia.dev@gmail.com>
2024-08-31 16:14:12 +0200

    fix: use io.open to write files
    when using File.write it fails to create new files

--- lua/neotest-java/util/write_file.lua
+++ lua/neotest-java/util/write_file.lua
@@ -6,7 +6,9 @@ local function write_file(filepath, content)
 	-- for os compatibibility
 	local compatible_path = filepath:gsub("\\", File.sep):gsub("/", File.sep)
 
-	return File.write(compatible_path, content)
+	local file = io.open(compatible_path, "w") or error(string.format("could not write to file %s", compatible_path))
+	file:write(content)
+	file:close()
 end
 
 return write_file
