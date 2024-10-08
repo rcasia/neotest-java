8254cd11e1a048293bf590dca07bddfeb8dd087f
Ricardo Casía <ricardocasia.dev@gmail.com>
2024-10-08 12:34:59 +0200

    fix: compatible paths for windows and unix (#158)
    Relates to #135

--- lua/neotest-java/util/compatible_path.lua
+++ lua/neotest-java/util/compatible_path.lua
@@ -1,9 +1,22 @@
-local Path = require("plenary.path")
-
 ---@param path string
 ---@return string compatible_path
 local function compatible_path(path)
-	return Path:new(path).filename
+	-- check if the system is windows
+	local has_win = vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1
+	-- set path separator based on the system
+	local sep = has_win and "\\" or "/"
+
+	-- preserve relative path prefix if present
+	local relative_prefix = ""
+	if path:sub(1, 2) == "./" or path:sub(1, 2) == ".\\" then
+		relative_prefix = "." .. sep
+	end
+
+	-- replace separators with the system's separator
+	path = path:gsub("%./", sep):gsub("%.\\", sep):gsub("/", sep):gsub("\\", sep)
+
+	-- normalize the path and remove duplicate separators
+	return (relative_prefix .. vim.fs.normalize(path)):gsub(sep .. "+", sep)
 end
 
 return compatible_path
