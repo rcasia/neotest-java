d8d0a32291f29ae345095a14697159c43b6425a7
Ricardo Casía Moka <ricardocasia.dev@gmail.com>
2024-10-13 10:55:36 +0200

    refactor: improve buffering in write_file function

--- lua/neotest-java/util/write_file.lua
+++ lua/neotest-java/util/write_file.lua
@@ -1,6 +1,9 @@
 local nio = require("nio")
 local compatible_path = require("neotest-java.util.compatible_path")
 local Path = require("plenary.path")
+local logger = require("neotest-java.logger")
+
+local BUFFER_SIZE = 500
 
 ---@param filepath string
 ---@param content string
@@ -10,17 +13,16 @@ local function write_file(filepath, content)
 	-- create parent directories if they don't exist
 	nio.fn.mkdir(Path:new(_filepath):parent():absolute(), "p")
 
+	logger.debug("writing to file: ", _filepath)
+
 	local file = io.open(_filepath, "w") or error("Could not open file for writing: " .. _filepath)
-	local buffer = ""
-	for i = 1, #content do
-		buffer = buffer .. content:sub(i, i)
-		if i % 500 == 0 then
-			file:write(buffer)
-			buffer = ""
-		end
+	local pointer = 1
+	for i = BUFFER_SIZE, #content, BUFFER_SIZE do
+		file:write(content:sub(pointer, i))
+		pointer = i + 1
 	end
-	if buffer ~= "" then
-		file:write(buffer)
+	if pointer <= #content then
+		file:write(content:sub(pointer))
 	end
 
 	file:close()
