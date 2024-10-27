c905ace90e5f90d8c37acf459699a5be8289b034
Ricardo Casía Moka <ricardocasia.dev@gmail.com>
2024-10-27 21:27:06 +0100

    test: resolve_qualified_name basic case

--- tests/util/resolve_qualified_name_spec.lua
+++ tests/util/resolve_qualified_name_spec.lua
@@ -1,20 +1,48 @@
----@diagnostic disable: undefined-field
-local async = require("nio").tests
+local a = require("nio").tests
 local resolve_qualified_name = require("neotest-java.util.resolve_qualified_name")
 
-local cwd = vim.fn.getcwd()
-local EXAMPLE_FILEPATH = cwd .. "/tests/fixtures/maven-demo/src/test/java/com/example/ExampleTest.java"
-local EXAMPLE_PACKAGE = "com.example.ExampleTest"
-local BAD_EXAMPLE_FILEPATH = cwd .. "/tests/fixtures/maven-demo/src/test/java/com/example/NonExistentTest.java"
+describe("resolve_qualified_name", function()
+	local tmp_files
 
-describe("resolve_qualified_name function", function()
-	async.it("it should resolve package from filename", function()
-		assert.are.equal(EXAMPLE_PACKAGE, resolve_qualified_name(EXAMPLE_FILEPATH))
+	before_each(function()
+		tmp_files = {}
 	end)
 
-	async.it("it should error when file does not exist", function()
-		assert.has_error(function()
-			resolve_qualified_name(BAD_EXAMPLE_FILEPATH)
-		end, string.format("file does not exist: %s", BAD_EXAMPLE_FILEPATH))
+	after_each(function()
+		-- clear temporary files
+		for _, file in ipairs(tmp_files) do
+			os.remove(file)
+		end
 	end)
+
+	---@param content string
+	---@return string filename
+	local function create_tmp_file(content)
+		local tmp_file = os.tmpname()
+		table.insert(tmp_files, tmp_file)
+		local file = assert(io.open(tmp_file, "w"))
+		file:write(content)
+		file:close()
+		return tmp_file
+	end
+
+	local testcases = {
+		{
+			input = [[
+    package com.example;
+
+    public class ExampleTest {}
+
+    ]],
+			expected = "com.example.ExampleTest",
+		},
+	}
+
+	for _, case in ipairs(testcases) do
+		a.it("should resolve the qualified name of a file", function()
+			local result = resolve_qualified_name(create_tmp_file(case.input))
+
+			assert.are.same(case.expected, result)
+		end)
+	end
 end)
