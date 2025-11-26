443158540dafc6c14687665774615e4dc6524d96
jojo <filhojoseildo@gmail.com>
2025-11-26 00:23:27 -0300

    chore: setups the root for the tests

--- tests/core/file_checker_spec.lua
+++ tests/core/file_checker_spec.lua
@@ -2,46 +2,49 @@ local plugin = require("neotest-java")
 local it = require("nio").tests.it -- async
 
 describe("file_checker", function()
-	it("should return true for test files", function()
-		local test_files = {
-			"src/test/java/neotest/NeotestTest.java",
-			"src/test/java/neotest/RepositoryTests.java",
-			"src/test/java/neotest/NeotestIT.java",
-			"src/test/java/neotest/ProductAceptanceTests.java",
-			"src/test/java/neotest/domain/ProductAceptanceTests.java",
-		}
+  it("should return true for test files", function()
+    local test_files = {
+      "src/test/java/neotest/NeotestTest.java",
+      "src/test/java/neotest/RepositoryTests.java",
+      "src/test/java/neotest/NeotestIT.java",
+      "src/test/java/neotest/ProductAceptanceTests.java",
+      "src/test/java/neotest/domain/ProductAceptanceTests.java",
+    }
 
-		for _, file_path in ipairs(test_files) do
-			assert.is_true(plugin.is_test_file(file_path))
-		end
-	end)
+    for _, file_path in ipairs(test_files) do
+      assert.is_true(plugin.is_test_file(file_path))
+    end
+  end)
 
-	it("should return false for a java non-test file", function()
-		local non_test_files = {
-			"src/test/java/neotest/Configuration.java",
-			"src/test/java/neotest/TestRepository.java",
-			"src/test/java/neotest/Neotest.java",
-		}
-		for _, file_path in ipairs(non_test_files) do
-			assert.is_false(plugin.is_test_file(file_path))
-		end
-	end)
+  it("should return false for a java non-test file", function()
+    local non_test_files = {
+      "src/test/java/neotest/Configuration.java",
+      "src/test/java/neotest/TestRepository.java",
+      "src/test/java/neotest/Neotest.java",
+    }
+    for _, file_path in ipairs(non_test_files) do
+      assert.is_false(plugin.is_test_file(file_path))
+    end
+  end)
 
-	it("should return false for every class inside main folder", function()
-		local non_test_files = {
-			"src/main/java/neotest/NeotestTest.java",
-		}
-		for _, file_path in ipairs(non_test_files) do
-			assert.is_false(plugin.is_test_file(file_path))
-		end
-	end)
+  it("should return false for every class inside main folder", function()
+    local non_test_files = {
+      "src/main/java/neotest/NeotestTest.java",
+    }
+    for _, file_path in ipairs(non_test_files) do
+      assert.is_false(plugin.is_test_file(file_path))
+    end
+  end)
 
   it("should return true if theres a /main/ outside the root path", function()
-		local non_test_files = {
-			"main/src/java/neotest/NeotestTest.java",
-		}
-		for _, file_path in ipairs(non_test_files) do
-			assert.is_true(plugin.is_test_file(file_path))
-		end
-	end)
+    local ch = require "neotest-java.context_holder"
+    ch.set_root '/absolute_path/main/src'
+    local non_test_files = {
+      "/absolute_path/main/src/java/neotest/NeotestTest.java",
+    }
+    for _, file_path in ipairs(non_test_files) do
+      assert.is_true(plugin.is_test_file(file_path))
+    end
+    ch.set_root ''
+  end)
 end)
