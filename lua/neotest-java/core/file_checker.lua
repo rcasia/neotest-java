0b4d22befe13c7b42483528d0b1b9ce9abbf04f7
joseildo filho <filhojoseildo@gmail.com>
2025-12-01 19:23:52 -0300

    chore: adds prints

--- lua/neotest-java/core/file_checker.lua
+++ lua/neotest-java/core/file_checker.lua
@@ -17,8 +17,10 @@ end
 ---@return boolean
 function FileChecker.is_test_file(file_path)
   file_path = compatible_path(file_path)
+  print(file_path)
 
   local root = compatible_path(ch.get_context().root or root_finder.find_root(vim.fn.getcwd(), matcher) or "")
+  print(root)
 
   local relative_path = path:new(file_path):make_relative(root)
   if string.find(relative_path, "/main/") then
