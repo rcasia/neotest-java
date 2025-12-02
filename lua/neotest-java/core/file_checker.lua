973c593d5b9f5dc19b9ff61ac87d882b7f987827
joseildo filho <filhojoseildo@gmail.com>
2025-12-02 17:28:22 -0300

    fix: make the file_checker sensible for the main before the root (#220)
    For some reason i had my java project inside a folder main, and that was
    causing the tests to be ignored. So, basically i created a PR to make
    the file_checker use only the relative path.

--- lua/neotest-java/core/file_checker.lua
+++ lua/neotest-java/core/file_checker.lua
@@ -1,20 +1,34 @@
 local JAVA_TEST_FILE_PATTERNS = require("neotest-java.types.patterns").JAVA_TEST_FILE_PATTERNS
+local root_finder = require("neotest-java.core.root_finder")
+local compatible_path = require("neotest-java.util.compatible_path")
+local ch = require("neotest-java.context_holder")
+local path = require("plenary.path")
 
 local FileChecker = {}
+
+local matcher = function(pattern)
+  return function(dir)
+    return string.find(dir, pattern)
+  end
+end
 
 ---@async
 ---@param file_path string
 ---@return boolean
 function FileChecker.is_test_file(file_path)
-	if string.find(file_path, "/main/") then
-		return false
-	end
-	for _, pattern in ipairs(JAVA_TEST_FILE_PATTERNS) do
-		if string.find(file_path, pattern) then
-			return true
-		end
-	end
-	return false
+  file_path = compatible_path(file_path)
+  local root = compatible_path(ch.get_context().root or root_finder.find_root(vim.fn.getcwd(), matcher) or "")
+
+  local relative_path = path:new(file_path):make_relative(root)
+  if string.find(relative_path, compatible_path("/main/")) then
+    return false
+  end
+  for _, pattern in ipairs(JAVA_TEST_FILE_PATTERNS) do
+    if string.find(relative_path, pattern) then
+      return true
+    end
+  end
+  return false
 end
 
 return FileChecker
