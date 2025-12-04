d81e11daeddc6aff713258fa422df8c3de5047c7
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-12-05 00:03:04 +0100

    refactor: create Path structure with basic methods (#222)

--- lua/neotest-java/util/compatible_path.lua
+++ lua/neotest-java/util/compatible_path.lua
@@ -1,22 +1,9 @@
+local Path = require("neotest-java.util.path")
+
 ---@param path string
 ---@return string compatible_path
 local function compatible_path(path)
-	-- check if the system is windows
-	local has_win = vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1
-	-- set path separator based on the system
-	local sep = has_win and "\\" or "/"
-
-	-- preserve relative path prefix if present
-	local relative_prefix = ""
-	if path:sub(1, 2) == "./" or path:sub(1, 2) == ".\\" then
-		relative_prefix = "." .. sep
-	end
-
-	-- replace separators with the system's separator
-	path = path:gsub("%./", sep):gsub("%.\\", sep):gsub("/", sep):gsub("\\", sep)
-
-	-- normalize the path and remove duplicate separators
-	return (relative_prefix .. path):gsub(sep .. "+", sep)
+	return Path(path).to_string()
 end
 
 return compatible_path
