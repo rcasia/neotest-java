6111d9ddb071d6803c6d420700f0ab364011f9c6
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-12-04 23:51:40 +0100

    refactor: take out everything inside compatible_path function

--- lua/neotest-java/util/compatible_path.lua
+++ lua/neotest-java/util/compatible_path.lua
@@ -3,18 +3,7 @@ local Path = require("neotest-java.util.path")
 ---@param path string
 ---@return string compatible_path
 local function compatible_path(path)
-	-- check if the system is windows
-	local has_win = vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1
-	-- set path separator based on the system
-	local sep = has_win and "\\" or "/"
-
-	-- preserve relative path prefix if present
-	local relative_prefix = ""
-	if path:sub(1, 2) == "./" or path:sub(1, 2) == ".\\" then
-		relative_prefix = "." .. sep
-	end
-
-	return Path(relative_prefix .. path).to_string()
+	return Path(path).to_string()
 end
 
 return compatible_path
