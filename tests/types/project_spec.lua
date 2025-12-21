f98d34a432eb83544a334d08c6b7ebeae9ccbcbd
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-12-21 17:35:11 +0100

    test: put input args together and make tests sync

--- tests/types/project_spec.lua
+++ tests/types/project_spec.lua
@@ -1,4 +1,3 @@
-local async = require("nio").tests
 local Project = require("neotest-java.types.project")
 local Path = require("neotest-java.util.path")
 
@@ -7,29 +6,35 @@ local eq = assert.are.same
 describe("project", function()
 	local testscases = {
 		{
-			input = Path("./tests/fixtures/maven-demo"),
-			dirs = {
-				Path("./tests/fixtures/maven-demo/pom.xml"),
+			input = {
+				root_dir = Path("./tests/fixtures/maven-demo"),
+				dirs = {
+					Path("./tests/fixtures/maven-demo/pom.xml"),
+				},
+				project_filename = "pom.xml",
 			},
-			project_filename = "pom.xml",
 			expected = {
 				{ name = "maven-demo", base_dir = Path("./tests/fixtures/maven-demo") },
 			},
 		},
 		{
-			input = Path("./tests/fixtures/gradle-groovy-demo"),
-			dirs = {
-				Path("./tests/fixtures/gradle-groovy-demo/pom.xml"),
+			input = {
+				root_dir = Path("./tests/fixtures/gradle-groovy-demo"),
+				dirs = {
+					Path("./tests/fixtures/gradle-groovy-demo/pom.xml"),
+				},
+				project_filename = "pom.xml",
 			},
-			project_filename = "pom.xml",
 			expected = {
 				{ name = "gradle-groovy-demo", base_dir = Path("./tests/fixtures/gradle-groovy-demo") },
 			},
 		},
 	}
 	for _, testcase in ipairs(testscases) do
-		async.it("should get modules: " .. testcase.input.to_string(), function()
-			local project = Project.from_root_dir(testcase.input, testcase.project_filename, testcase.dirs)
+		it("should get modules: " .. testcase.input.root_dir.to_string(), function()
+			local project =
+				Project.from_root_dir(testcase.input.root_dir, testcase.input.project_filename, testcase.input.dirs)
+
 			local results = {}
 			for _, mod in ipairs(project:get_modules()) do
 				results[#results + 1] = { name = mod.name, base_dir = mod.base_dir }
