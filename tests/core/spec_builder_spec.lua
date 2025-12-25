f38f84bc191e0fc1ccdc93f9f5e1a585216d4a01
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-12-25 20:33:40 +0100

    test: builds spec for one method in a multi-module project

--- tests/core/spec_builder_spec.lua
+++ tests/core/spec_builder_spec.lua
@@ -81,4 +81,75 @@ describe("SpecBuilder", function()
 			symbol = "shouldNotFail",
 		}, actual)
 	end)
+
+	it("builds spec for one method in a multi-module project", function()
+		local args = mock_args_tree({
+			id = "com.example.ExampleInSecondModuleTest#shouldNotFail()",
+			path = "/user/home/root/module-2/src/test/java/com/example/ExampleInSecondModuleTest.java",
+			name = "shouldNotFail",
+			type = "test",
+		})
+		local config = {
+			junit_jar = Path("my-junit-jar.jar"),
+		}
+		local project_paths = {
+			Path("/user/home/root"),
+			Path("/user/home/root/pom.xml"),
+			Path("/user/home/root/module-1/pom.xml"),
+			Path("/user/home/root/module-1/src/test/java/com/example/ExampleTest.java"),
+			Path("/user/home/root/module-2/pom.xml"),
+			Path("/user/home/root/module-2/src/test/java/com/example/ExampleInSecondModuleTest.java"),
+		}
+		local expected_base_dir = Path("/user/home/root/module-2").to_string()
+
+		-- when
+		local actual = SpecBuilder.build_spec(args, config, {
+			mkdir = function() end,
+			chdir = function() end,
+			root_getter = function()
+				return Path("root")
+			end,
+			scan = function()
+				return project_paths
+			end,
+			compile = function(base_dir)
+				assert(base_dir == expected_base_dir, "should compile with the expected_base_dir")
+				return "classpath-file-argument"
+			end,
+			report_folder_name_gen = function()
+				return Path("report_folder")
+			end,
+			build_tool_getter = function()
+				--- @type neotest-java.BuildTool
+				return FakeBuildTool
+			end,
+			detect_project_type = function()
+				return "maven"
+			end,
+		})
+
+		-- then
+		eq({
+			command = vim.iter({
+				"java",
+				"-Dspring.config.additional-location=",
+				"-jar",
+				"my-junit-jar.jar",
+				"execute",
+				"--classpath=classpath-file-argument",
+				"--reports-dir=report_folder",
+				"--fail-if-no-tests",
+				"--disable-banner",
+				"--details=testfeed",
+				"--config=junit.platform.output.capture.stdout=true",
+				"--select-class='com.example.ExampleInSecondModuleTest'",
+				"--select-method='com.example.ExampleInSecondModuleTest#shouldNotFail()'",
+			}):join(" "),
+			context = {
+				reports_dir = Path("report_folder"),
+			},
+			cwd = "root",
+			symbol = "shouldNotFail",
+		}, actual)
+	end)
 end)
