a49ac35feb05f43312c623be49760a9dfe32138d
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2026-01-02 01:08:50 +0100

    refactor: remove unused modules

--- lua/neotest-java/util/compatible_path.lua
+++
@@ -1,9 +0,0 @@
-local Path = require("neotest-java.model.path")
-
----@param path string
----@return string compatible_path
-local function compatible_path(path)
-	return Path(path).to_string()
-end
-
-return compatible_path
