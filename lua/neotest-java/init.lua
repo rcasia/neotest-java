af626101933dd8365dec020b0862220ee9ad92ab
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2026-01-05 00:32:49 +0100

    refactor: move out the jar installation function

--- lua/neotest-java/init.lua
+++ lua/neotest-java/init.lua
@@ -10,9 +10,8 @@ local log = require("neotest-java.logger")
 local ch = require("neotest-java.context_holder")
 local Path = require("neotest-java.model.path")
 local nio = require("nio")
-local exists = require("neotest.lib.file").exists
 local logger = require("neotest-java.logger")
-local lib = require("neotest.lib")
+local install = require("neotest-java.install")
 
 local DEFAULT_CONFIG = require("neotest-java.default_config")
 
@@ -35,38 +34,6 @@ end
 
 local chdir = function(dir)
 	nio.fn.chdir(dir:to_string())
-end
-
---- @param config neotest-java.ConfigOpts
-local install = function(config)
-	local filepath = config.junit_jar:to_string()
-
-	if exists(filepath) then
-		lib.notify("Already setup!")
-		return
-	end
-
-	vim.system(
-		{
-			"curl",
-			"--output",
-			filepath,
-			("https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/%s/junit-platform-console-standalone-%s.jar"):format(
-				config.default_version,
-				config.default_version
-			),
-			"--create-dirs",
-		},
-		nil,
-		function(out)
-			if out.code == 0 then
-				lib.notify("Downloaded Junit Standalone successfully!")
-			else
-				lib.notify(string.format("Error while downloading: \n %s", out.stderr), "error")
-				logger.error(out.stderr)
-			end
-		end
-	)
 end
 
 --- @class neotest-java.Adapter : neotest.Adapter
---
+++ lua/neotest-java/install.lua
@@ -0,0 +1,37 @@
+local exists = require("neotest.lib.file").exists
+local lib = require("neotest.lib")
+local logger = require("neotest-java.logger")
+
+--- @param config neotest-java.ConfigOpts
+local install = function(config)
+	local filepath = config.junit_jar:to_string()
+
+	if exists(filepath) then
+		lib.notify("Already setup!")
+		return
+	end
+
+	vim.system(
+		{
+			"curl",
+			"--output",
+			filepath,
+			("https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/%s/junit-platform-console-standalone-%s.jar"):format(
+				config.default_version,
+				config.default_version
+			),
+			"--create-dirs",
+		},
+		nil,
+		function(out)
+			if out.code == 0 then
+				lib.notify("Downloaded Junit Standalone successfully!")
+			else
+				lib.notify(string.format("Error while downloading: \n %s", out.stderr), "error")
+				logger.error(out.stderr)
+			end
+		end
+	)
+end
+
+return install
