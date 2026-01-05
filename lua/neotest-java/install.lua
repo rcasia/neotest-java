95de40112b305aec140e91ea35e8f64fc16e4b21
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2026-01-05 01:39:01 +0100

    feat: add checksum to automatic installation of junit jar

--- lua/neotest-java/install.lua
+++ lua/neotest-java/install.lua
@@ -2,6 +2,16 @@ local exists = require("neotest.lib.file").exists
 local lib = require("neotest.lib")
 local logger = require("neotest-java.logger")
 
+--- @param file_path neotest-java.Path
+--- @return string hash
+local checksum = function(file_path)
+	local f = assert(io.open(file_path:to_string(), "rb"))
+	local data = f:read("*a")
+	f:close()
+	local hash = vim.fn.sha256(data)
+	return hash
+end
+
 --- @param config neotest-java.ConfigOpts
 local install = function(config)
 	local filepath = config.junit_jar:to_string()
@@ -10,30 +20,39 @@ local install = function(config)
 		lib.notify("Already setup!")
 		return
 	end
+	local default_junit_jar_filepath = config.default_junit_jar_filepath:to_string()
 
-	vim.system(
-		{
-			"curl",
-			"--output",
-			config.default_junit_jar_filepath:to_string(),
-			("https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/%s/junit-platform-console-standalone-%s.jar"):format(
-				config.default_version,
-				config.default_version
-			),
-			"--create-dirs",
-		},
-		nil,
-		function(out)
-			if out.code == 0 then
-				lib.notify(
-					"Downloaded Junit Standalone successfully at: \n" .. config.default_junit_jar_filepath:to_string()
-				)
-			else
-				lib.notify(string.format("Error while downloading: \n %s", out.stderr), "error")
-				logger.error(out.stderr)
-			end
-		end
-	)
+	local out = vim.system({
+		"curl",
+		"--output",
+		default_junit_jar_filepath,
+		("https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/%s/junit-platform-console-standalone-%s.jar"):format(
+			config.default_junit_jar_version.version,
+			config.default_junit_jar_version.version
+		),
+		"--create-dirs",
+	}):wait(10000)
+
+	if out.code ~= 0 then
+		lib.notify(string.format("Error while downloading: \n %s", out.stderr), "error")
+		logger.error(out.stderr)
+		return
+	end
+
+	local sha = checksum(config.default_junit_jar_filepath)
+	local expected_sha = config.default_junit_jar_version.sha256
+	if sha ~= expected_sha then
+		local message = ([[
+			Checksum verification failed!
+			Expected: %s
+			Got:      %s
+		]]):format(expected_sha, sha)
+
+		lib.notify(message, "error")
+		logger.error(message)
+		return
+	end
+	lib.notify("Downloaded Junit Standalone successfully at: \n" .. default_junit_jar_filepath)
 end
 
 return install
