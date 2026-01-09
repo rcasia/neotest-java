3f8b1fa49815c81f7352e674dc0fcf688e38d54d
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2026-01-09 20:24:34 +0100

    refactor: simplify namespace_id logic

--- lua/neotest-java/core/namespace_id.lua
+++ lua/neotest-java/core/namespace_id.lua
@@ -1,24 +1,54 @@
+--- @param str string
+--- @return boolean
+local not_empty = function(str)
+	return str ~= nil and str ~= ""
+end
+
+--- @param str string
+--- @return boolean
+local is_empty = function(str)
+	return str == nil or str == ""
+end
+
+---@param position neotest.Position
+---@return boolean
+local is_namespace = function(position)
+	return position.type == "namespace"
+end
+
+--- @param key string
+--- @return fun(t: table): any
+local property = function(key)
+	return function(t)
+		assert(type(t) == "table", "Expected a table")
+		return t[key]
+	end
+end
+
 --- @param position neotest.Position
 --- @param parents neotest.Position[]
 --- @param package_name string
 local namespace_id = function(position, parents, package_name)
 	local namespace_string = vim
+		--
 		.iter(parents)
-		--- @param pos neotest.Position
-		:filter(function(pos)
-			return pos.type == "namespace"
-		end)
-		:map(function(pos)
-			return pos.name
-		end)
+		:filter(is_namespace)
+		:map(property("name"))
 		:join("$")
 
-	if namespace_string == "" then
-		return package_name ~= "" and (package_name .. "." .. position.name) or position.name
+	if is_empty(namespace_string) then
+		return vim
+			--
+			.iter({ package_name, position.name })
+			:filter(not_empty)
+			:join(".")
 	end
 
-	return package_name ~= "" and (package_name .. "." .. namespace_string .. "$" .. position.name)
-		or (namespace_string .. "$" .. position.name)
+	return vim
+		--
+		.iter({ package_name, namespace_string .. "$" .. position.name })
+		:filter(not_empty)
+		:join(".")
 end
 
 return namespace_id
