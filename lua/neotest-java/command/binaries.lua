10bc3d36759c6cf27b2d78108f7f3016e92316a9
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2026-01-10 16:36:59 +0100

    feat: expose jdtls javap binary

--- lua/neotest-java/command/binaries.lua
+++ lua/neotest-java/command/binaries.lua
@@ -4,30 +4,38 @@ local logger = require("neotest-java.logger")
 
 --- @class neotest-java.LspBinaries
 --- @field java fun(cwd: neotest-java.Path): neotest-java.Path
+--- @field javap fun(cwd: neotest-java.Path): neotest-java.Path
 
 --- @param deps { client_provider: fun(cwd: neotest-java.Path): vim.lsp.Client }
 --- @return neotest-java.LspBinaries
 local Binaries = function(deps)
+	local get_java_home = function(cwd)
+		local client = deps.client_provider(cwd)
+
+		logger.debug("Resolving Java home via JDTLS for cwd: " .. cwd:to_string())
+
+		local cmd = {
+
+			command = "java.project.getSettings",
+			arguments = { vim.uri_from_fname(cwd:to_string()), { "org.eclipse.jdt.ls.core.vm.location" } },
+		}
+		local res = client:request_sync("workspace/executeCommand", cmd)
+		assert(res, "No response from lsp server when getting Java home.")
+		assert(not res.err, "Error while getting Java home from lsp server: " .. vim.inspect(res.err))
+
+		return res.result["org.eclipse.jdt.ls.core.vm.location"]
+	end
+
 	return {
 
 		--- @param cwd neotest-java.Path
 		java = function(cwd)
-			local client = deps.client_provider(cwd)
+			return Path(get_java_home(cwd)):append("/bin/java")
+		end,
 
-			logger.debug("Resolving Java binary via JDTLS for cwd: " .. cwd:to_string())
-
-			local cmd = {
-
-				command = "java.project.getSettings",
-				arguments = { vim.uri_from_fname(cwd:to_string()), { "org.eclipse.jdt.ls.core.vm.location" } },
-			}
-			local res = client:request_sync("workspace/executeCommand", cmd)
-			assert(res, "No response from lsp server when getting Java home.")
-			assert(not res.err, "Error while getting Java home from lsp server: " .. vim.inspect(res.err))
-
-			local jdtls_java_home = res.result["org.eclipse.jdt.ls.core.vm.location"]
-
-			return Path(jdtls_java_home):append("/bin/java")
+		--- @param cwd neotest-java.Path
+		javap = function(cwd)
+			return Path(get_java_home(cwd)):append("/bin/javap")
 		end,
 	}
 end
--- tests/unit/binaries_spec.lua
+++ tests/unit/binaries_spec.lua
@@ -5,25 +5,31 @@ local Binaries = require("neotest-java.command.binaries")
 local Path = require("neotest-java.model.path")
 
 describe("Binaries", function()
-	it("works", function()
-		local expected_cwd = Path("some")
+	local expected_cwd = Path("some")
 
-		local test_client_provider = function(cwd)
-			eq(expected_cwd, cwd)
-			return {
-				request_sync = function(_, method, params)
-					eq(method, "workspace/executeCommand")
-					eq(params.command, "java.project.getSettings")
-					eq(params.arguments[1], "file://some")
-					eq(params.arguments[2], { "org.eclipse.jdt.ls.core.vm.location" })
+	local test_client_provider = function(cwd)
+		eq(expected_cwd, cwd)
+		return {
+			request_sync = function(_, method, params)
+				eq(method, "workspace/executeCommand")
+				eq(params.command, "java.project.getSettings")
+				eq(params.arguments[1], "file://some")
+				eq(params.arguments[2], { "org.eclipse.jdt.ls.core.vm.location" })
 
-					return { result = { ["org.eclipse.jdt.ls.core.vm.location"] = "my_java_home" } }
-				end,
-			}
-		end
+				return { result = { ["org.eclipse.jdt.ls.core.vm.location"] = "my_java_home" } }
+			end,
+		}
+	end
 
+	it("resolves jdtls java binary", function()
 		local bin = Binaries({ client_provider = test_client_provider })
 		local result = bin.java(expected_cwd)
 		eq(Path("my_java_home/bin/java"), result)
+	end)
+
+	it("resolves jdtls javap binary", function()
+		local bin = Binaries({ client_provider = test_client_provider })
+		local result = bin.javap(expected_cwd)
+		eq(Path("my_java_home/bin/javap"), result)
 	end)
 end)
