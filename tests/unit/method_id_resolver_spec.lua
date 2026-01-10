f24b360acc42e30f53e868eb2f91609520646def
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2026-01-10 16:31:09 +0100

    test: check more method ids are correctly resolved
    Related to #239

--- tests/unit/method_id_resolver_spec.lua
+++ tests/unit/method_id_resolver_spec.lua
@@ -52,13 +52,26 @@ describe("Method Id Resolver", function()
 	}
 	local fake_command_executor_invocations
 
-	--- @type neotest-java.CommandExecutor
-	local fake_command_executor = {
-		execute_command = function(command, args)
-			fake_command_executor_invocations[#fake_command_executor_invocations + 1] =
-				{ command = command, args = args }
+	--- @return neotest-java.CommandExecutor
+	local fake_command_executor = function(expected_result)
+		return {
+			execute_command = function(command, args)
+				fake_command_executor_invocations[#fake_command_executor_invocations + 1] =
+					{ command = command, args = args }
 
-			return {
+				return expected_result
+			end,
+		}
+	end
+
+	before_each(function()
+		fake_command_executor_invocations = {}
+	end)
+
+	it("runs javap command correctly", function()
+		local resolver = MethodIdResolver({ --
+			classpath_provider = fake_classpath_provider,
+			command_executor = fake_command_executor({
 				stdout = [[
     Compiled from "Something1Test.java"
     public class com.example.application.Something1Test {
@@ -74,30 +87,50 @@ describe("Method Id Resolver", function()
 				]],
 				stderr = "",
 				exit_code = 0,
-			}
-		end,
-	}
-
-	before_each(function()
-		fake_command_executor_invocations = {}
-	end)
-
-	it("works", function()
-		local resolver = MethodIdResolver({ --
-			classpath_provider = fake_classpath_provider,
-			command_executor = fake_command_executor,
+			}),
 		})
 
-		local result = resolver.resolve_complete_method_id("com.example.ExampleTest", "someMonths_scv")
+		resolver.resolve_complete_method_id("com.example.ExampleTest", "someMonths_scv")
 
 		eq(1, #fake_command_executor_invocations, "command_executor should be invoked once")
 		eq(
 			{ command = "bash", args = { "-c", "javap", "-cp=my_classpath", "com.example.ExampleTest" } },
 			fake_command_executor_invocations[1]
 		)
+	end)
+
+	it("resolves the complete method ids", function()
+		local resolver = MethodIdResolver({ --
+			classpath_provider = fake_classpath_provider,
+			command_executor = fake_command_executor({
+				stdout = [[
+    Compiled from "Something1Test.java"
+    public class com.example.application.Something1Test {
+        public com.example.application.Something1Test();
+        void testSomething();
+        void testSomething1();
+        void someMonths_scv(int, java.lang.String, java.lang.Integer, com.example.application.model.TestArgs$Role);
+        void someMonths_enum(com.example.application.Something1Test$TestMonth);
+        void testPersonFromCsv(com.example.model.Person);
+        void testPersonAge(com.example.model.Person);
+        static java.util.stream.Stream<com.example.model.Person> personProvider();
+    }
+				]],
+				stderr = "",
+				exit_code = 0,
+			}),
+		})
+
 		eq(
 			"someMonths_scv(int, java.lang.String, java.lang.Integer, com.example.application.model.TestArgs$Role)",
-			result
+			resolver.resolve_complete_method_id("com.example.ExampleTest", "someMonths_scv")
 		)
+		eq("testSomething()", resolver.resolve_complete_method_id("com.example.ExampleTest", "testSomething"))
+		eq("testSomething1()", resolver.resolve_complete_method_id("com.example.ExampleTest", "testSomething1"))
+		eq(
+			"testPersonAge(com.example.model.Person)",
+			resolver.resolve_complete_method_id("com.example.ExampleTest", "testPersonAge")
+		)
+		eq("personProvider()", resolver.resolve_complete_method_id("com.example.ExampleTest", "personProvider"))
 	end)
 end)
