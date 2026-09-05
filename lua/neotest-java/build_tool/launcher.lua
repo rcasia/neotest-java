6694449a392225731ab6793bbf4a990cefc44149
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2026-09-05 15:25:34 +0200

    test: strengthen coverage of launcher/detect_project_type ahead of plenary.nvim removal (#317) (#328)

--- lua/neotest-java/build_tool/launcher.lua
+++ lua/neotest-java/build_tool/launcher.lua
@@ -1,71 +1,108 @@
 local log = require("neotest-java.logger")
 local nio = require("nio")
-local Job = require("plenary.job")
 local lib = require("neotest.lib")
 
-local _, repl = pcall(require, "dap.repl")
+--- A minimal shape of the job runner this module needs. The default
+--- implementation is backed by `plenary.job`, but it can be swapped out
+--- (e.g. in tests, or by a future implementation that drops the
+--- plenary.nvim dependency) as long as it honors this contract:
+---   * `new(opts)` accepts the same callback fields plenary.Job does
+---     (`command`, `cwd`, `args`, `on_stderr`, `on_stdout`, `on_exit`)
+---   * the returned job exposes a `start()` method
+--- @class neotest-java.JobRunner
+--- @field new fun(self: neotest-java.JobRunner, opts: table): { start: fun() }
+
+---@param deps neotest-java.BuildToolLauncherDeps | nil
+---@return neotest-java.BuildToolLauncher
+local function BuildToolLauncher(deps)
+	deps = deps or {}
+	deps.job_runner = deps.job_runner or require("plenary.job")
+	deps.dap_repl = deps.dap_repl
+	if deps.dap_repl == nil then
+		local _, repl = pcall(require, "dap.repl")
+		deps.dap_repl = repl
+	end
+
+	return {
+		---@param command string
+		---@param args string[]
+		---@param cwd neotest-java.Path
+		---@return nio.control.Event
+		launch_debug_test = function(command, args, cwd)
+			lib.notify("Running debug test", vim.log.levels.INFO)
+			log.trace("run_debug_test function")
+
+			local repl = deps.dap_repl
+
+			local test_command_started_listening = nio.control.event()
+			local terminated_command_event = nio.control.event()
+
+			local stderr = {}
+			local job = deps.job_runner:new({
+				command = command,
+				cwd = cwd:to_string(),
+				args = args,
+				on_stderr = function(_, data)
+					if data == nil then
+						return
+					end
+					stderr[#stderr + 1] = data
+					if repl then
+						vim.schedule(function()
+							repl.append(data)
+						end)
+					end
+				end,
+				on_stdout = function(_, data)
+					if data == nil then
+						return
+					end
+					if string.find(data, "Listening") then
+						test_command_started_listening.set()
+					end
+					if repl then
+						vim.schedule(function()
+							repl.append(data)
+						end)
+					end
+				end,
+				on_exit = function(_, code)
+					terminated_command_event.set()
+
+					log.debug("command exited with code: ", code)
+					if code ~= 0 then
+						log.error("command exited with code: ", code)
+						log.error("stderr: ", table.concat(stderr, "\n"))
+					end
+				end,
+			})
+			log.debug("starting job with command: ", command, " ", table.concat(args, " "))
+			if repl then
+				repl.clear()
+			end
+			job:start()
+			test_command_started_listening.wait()
+
+			return terminated_command_event
+		end,
+	}
+end
+
+---@class neotest-java.BuildToolLauncherDeps
+---@field job_runner? neotest-java.JobRunner
+---@field dap_repl? false | { append: fun(data: string), clear: fun() }
 
 ---@class neotest-java.BuildToolLauncher
-local launcher = {}
+---@field launch_debug_test fun(command: string, args: string[], cwd: neotest-java.Path): nio.control.Event
+---@field new fun(deps: neotest-java.BuildToolLauncherDeps | nil): neotest-java.BuildToolLauncher
 
----@param command string
----@param args string[]
----@param cwd neotest-java.Path
----@return nio.control.Event
-function launcher.launch_debug_test(command, args, cwd)
-	lib.notify("Running debug test", vim.log.levels.INFO)
-	log.trace("run_debug_test function")
+-- Module-level singleton, matching the previous `launcher.launch_debug_test`
+-- call site contract (`require("neotest-java.build_tool.launcher").launch_debug_test(...)`).
+local launcher = BuildToolLauncher()
 
-	local test_command_started_listening = nio.control.event()
-	local terminated_command_event = nio.control.event()
-
-	local stderr = {}
-	local job = Job:new({
-		command = command,
-		cwd = cwd:to_string(),
-		args = args,
-		on_stderr = function(_, data)
-			if data == nil then
-				return
-			end
-			stderr[#stderr + 1] = data
-			if repl then
-				vim.schedule(function()
-					repl.append(data)
-				end)
-			end
-		end,
-		on_stdout = function(_, data)
-			if data == nil then
-				return
-			end
-			if string.find(data, "Listening") then
-				test_command_started_listening.set()
-			end
-			if repl then
-				vim.schedule(function()
-					repl.append(data)
-				end)
-			end
-		end,
-		on_exit = function(_, code)
-			terminated_command_event.set()
-
-			log.debug("command exited with code: ", code)
-			if code ~= 0 then
-				log.error("command exited with code: ", code)
-				log.error("stderr: ", table.concat(stderr, "\n"))
-			end
-		end,
-	})
-	log.debug("starting job with command: ", command, " ", table.concat(args, " "))
-	if repl then
-		repl.clear()
-	end
-	job:start()
-	test_command_started_listening.wait()
-
-	return terminated_command_event
-end
+-- Exposed for tests / future callers that want to inject a custom job_runner
+-- (e.g. a stub, or a non-plenary implementation) without going through the
+-- module-level singleton.
+launcher.new = BuildToolLauncher
 
 return launcher
